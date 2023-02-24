7237cb0e16d2fdc3f2a9419bf1f521a2990010d4
Hardik Hajela <hardik.hajela@icloud.com>
2023-02-24 03:22:50 -0500

    v1.5.7.1
    progress not visible

--- assets/js/script.js
+++ assets/js/script.js
@@ -670,8 +670,11 @@ function wait(sec) {
 
 
 //Progress Bar
+var offsetStart = 0;
+var offsetEnd = 0;
 
-
-
+window.addEventListener('scroll', () => {
+	document.documentElement.style.setProperty('--scroll', (window.pageYOffset - offsetStart) / (document.body.offsetHeight - offsetStart - offsetEnd - window.innerHeight));
+}, false);
 //Progress Bar
 
