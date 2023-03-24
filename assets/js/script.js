3488e82a13685ffe9d592848e6ebe75eb37476ad
Hardik Hajela <hardik.hajela@icloud.com>
2023-03-24 00:15:09 -0400

    v1.5.7.4.3

--- assets/js/script.js
+++ assets/js/script.js
@@ -668,7 +668,6 @@ function wait(sec) {
 
 // ================================================================================================================
 
-
 //Progress Bar
 var offsetStart = 0;
 var offsetEnd = 0;
@@ -677,5 +676,3 @@ window.addEventListener('scroll', () => {
 	document.documentElement.style.setProperty('--scroll', (window.pageYOffset - offsetStart) / (document.body.offsetHeight - offsetStart - offsetEnd - window.innerHeight));
 }, false);
 //Progress Bar
-
-
